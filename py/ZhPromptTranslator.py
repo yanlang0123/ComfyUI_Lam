a5e39a51459bece6862d09ce2eb6f14f905466d9
严浪 <757078144@qq.com>
2024-01-19 13:32:06 +0800

    插件优化

--- py/ZhPromptTranslator.py
+++ py/ZhPromptTranslator.py
@@ -6,34 +6,10 @@ import string
 from collections import OrderedDict
 from transformers import MarianMTModel,MarianTokenizer
 
-
-
 #获取组节点
 confdir = os.path.abspath(os.path.join(__file__, "../../config"))
 if not os.path.exists(confdir):
     os.mkdir(confdir)
-
-my_translations = os.path.join(confdir,"translations.csv")
-model = MarianMTModel.from_pretrained('Helsinki-NLP/opus-mt-zh-en').cuda()
-tokenizer = MarianTokenizer.from_pretrained('Helsinki-NLP/opus-mt-zh-en')
-tokenizer.src_lang = "zh_CN"
-
-
-def translate(chinese_str: str) -> str:
-    # 对中文句子进行分词
-    input_ids = tokenizer.encode(chinese_str, return_tensors="pt").cuda()
-
-    # 进行翻译
-    output_ids = model.generate(input_ids)
-
-    # 将翻译结果转换为字符串格式
-    english_str = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-    #如果最后有一个.，则去掉
-    if english_str[-1] == '.':
-        english_str = english_str[:-1]
-    return english_str
-
-
 
 def sort_dict_by_key_length(d):
     sorted_keys = sorted(d.keys(), key=lambda x: len(x),reverse=True)
@@ -41,9 +17,6 @@ def sort_dict_by_key_length(d):
     for key in sorted_keys:
         sorted_dict[key] = d[key]
     return sorted_dict
-
-
-
 
 # 读取 csv 文件到内存中缓存起来
 def load_csv(csv_file):
@@ -54,41 +27,15 @@ def load_csv(csv_file):
 
     return cache
 
-
-
-
 def contains_chinese(text):
     pattern = re.compile(r'[\u4e00-\u9fa5]')
     return bool(pattern.search(text))
-
-
-
 
 def remove_unnecessary_spaces(text):
     """Removes unnecessary spaces between characters."""
     pattern = r"\)\s*\+\+|\)\+\+\s*"
     replacement = r")++"
     return re.sub(pattern, replacement, text)
-
-
-def process_text(text):
-    # 将中文全角标点符号替换为半角标点符号
-    text = text.translate(str.maketrans('，。！？；：‘’“”（）【】', ',.!?;:\'\'\"\"()[]'))
-    # 按逗号分割成数组
-    text_array = text.split(',')
-    # 对数组中每个字符串进行处理
-    for i in range(len(text_array)):
-        # 如果字符串以 < 开头 > 结尾，则是Lora，跳过不处理
-        if text_array[i].startswith('<') and text_array[i].endswith('>'):
-            continue
-        # 判断是否只包含英文字符
-        if all(char in string.printable + ' ' for char in text_array[i]):
-            continue
-        else:
-            # 调用 transfer 函数进行翻译
-            text_array[i] = translate(text_array[i])
-    # 重新用逗号连接成字符串并返回
-    return ','.join(text_array)
 
 
 
@@ -102,6 +49,45 @@ def replace_text(text, cache):
 
 
 class ZhPromptTranslator:
+
+    def __init__(self):
+        self.my_translations = os.path.join(confdir,"translations.csv")
+        self.model = MarianMTModel.from_pretrained('Helsinki-NLP/opus-mt-zh-en').cuda()
+        self.tokenizer = MarianTokenizer.from_pretrained('Helsinki-NLP/opus-mt-zh-en')
+        self.tokenizer.src_lang = "zh_CN"
+    def translate(self,chinese_str: str) -> str:
+        # 对中文句子进行分词
+        input_ids = self.tokenizer.encode(chinese_str, return_tensors="pt").cuda()
+
+        # 进行翻译
+        output_ids = self.model.generate(input_ids)
+
+        # 将翻译结果转换为字符串格式
+        english_str = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
+        #如果最后有一个.，则去掉
+        if english_str[-1] == '.':
+            english_str = english_str[:-1]
+        return english_str
+    
+    def process_text(self,text):
+        # 将中文全角标点符号替换为半角标点符号
+        text = text.translate(str.maketrans('，。！？；：‘’“”（）【】', ',.!?;:\'\'\"\"()[]'))
+        # 按逗号分割成数组
+        text_array = text.split(',')
+        # 对数组中每个字符串进行处理
+        for i in range(len(text_array)):
+            # 如果字符串以 < 开头 > 结尾，则是Lora，跳过不处理
+            if text_array[i].startswith('<') and text_array[i].endswith('>'):
+                continue
+            # 判断是否只包含英文字符
+            if all(char in string.printable + ' ' for char in text_array[i]):
+                continue
+            else:
+                # 调用 transfer 函数进行翻译
+                text_array[i] = self.translate(text_array[i])
+        # 重新用逗号连接成字符串并返回
+        return ','.join(text_array)
+    
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -124,14 +110,14 @@ class ZhPromptTranslator:
 
         print("prompt: ", text_trans)
 
-        cache = load_csv(my_translations)
+        cache = load_csv(self.my_translations)
 
         if contains_chinese(text_trans):
             text_trans = remove_unnecessary_spaces(text_trans)
             modified_text = replace_text(text_trans, cache)
             print("modified_text: " + modified_text)
 
-            target_text = process_text(modified_text)
+            target_text = self.process_text(modified_text)
             target_text = re.sub('♪','', target_text)
         else:
             target_text = text_trans
