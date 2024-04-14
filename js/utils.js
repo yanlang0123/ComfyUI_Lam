7f28088ffc6f23f7695ed75b62ae8fb594ebabf7
严浪 <757078144@qq.com>
2024-04-14 15:59:53 +0800

    BUG修改

--- js/utils.js
+++ js/utils.js
@@ -227,6 +227,12 @@ export function getDrawColor(percent, alpha) {
 	return `#${f(0)}${f(8)}${f(4)}${alpha}`;
 }
 
+export function hexToRgba(hex, opacity) {
+	return hex && hex.replace(/\s+/g, '').length === 7 ? 'rgba(' + parseInt('0x' + hex.slice(1, 3)) + ',' +
+	  parseInt('0x' + hex.slice(3, 5)) + ',' +
+	  parseInt('0x' + hex.slice(5, 7)) + ',' + opacity + ')' : ''
+}
+
 export function computeCanvasSize(node, size) {
 	if (node.widgets[0].last_y == null) return;
 
