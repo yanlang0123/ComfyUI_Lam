357ccd2b4364a7a91fd93a2e5b3b73604f6a46bc
yanlang0123 <yanlang0123@gmail.com>
2024-08-16 16:04:45 +0800

    计次循环优化升级

--- py/ForEnd.py
+++ py/ForEnd.py
@@ -3,6 +3,9 @@ import numpy as np
 import requests
 import json
 from .src.utils.uitls import AlwaysEqualProxy
+from server import PromptServer
+from .WeChatAuth import prompt as promptFun
+from copy import deepcopy
 
 class ForEnd:
     def __init__(self):
@@ -15,8 +18,12 @@ class ForEnd:
             "required": {
                 "total": ("INT", {"forceInput": True}),
                 "i": ("INT",{"forceInput": True}),
-                "port": ("INT", {"default": 8188, "min": 1, "max": 99999}),
                 "obj": (AlwaysEqualProxy("*"),),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",           #节点编号
+                "prompt": "PROMPT",                 #流程节点信息
+                "extra_pnginfo": "EXTRA_PNGINFO"    #前端流程图信息
             },
         }
     RETURN_TYPES = ()
@@ -25,32 +32,34 @@ class ForEnd:
 
     CATEGORY = "lam"
 
-    def for_end_fun(self,total,i,port,obj):
-        
-        r = requests.get("http://127.0.0.1:"+str(port)+"/queue")
-        result = r.text
-        #print(result)
-        data = json.loads(result)
-        #rdata=data[list(data.keys())[0]]
-        rdata=data['queue_running'][0] if len(data['queue_running'])>0 else []
-        index=0
-        if len(rdata)>0:
-            pdata=json.loads('{}')
-            pdata['client_id']=rdata[3]['client_id']
-            pdata['extra_data']={'extra_pnginfo':rdata[3]['extra_pnginfo']}
-            pdata['prompt']=rdata[2]
-            for key in list(pdata['prompt'].keys()):
-                if pdata['prompt'][key]['class_type']=='ForStart':
-                    index=i//pdata['prompt'][key]['inputs']['stop']
-                    i=i+pdata['prompt'][key]['inputs']['stop']
-                    pdata['prompt'][key]['inputs']['i']=i
-                    break
-            if i>=total:
-                return { "ui": { "text":'循环结束' } }
-            r = requests.post("http://127.0.0.1:"+str(port)+"/prompt",json=pdata)
-            result = r.text
+    def for_end_fun(self,total,i,obj,unique_id,prompt,extra_pnginfo):
+        pdata=json.loads('{}')
+        pdata['client_id']=PromptServer.instance.client_id
+        pdata['extra_data']={'extra_pnginfo':deepcopy(extra_pnginfo)}
+        pdata['prompt']=deepcopy(prompt)
+        start_id=prompt[unique_id]['inputs']['total'][0]
+        StatusInfo=pdata['extra_data']['extra_pnginfo']['StatusInfo'] if 'StatusInfo' in pdata['extra_data']['extra_pnginfo'] else ''
+        index=pdata['extra_data']['extra_pnginfo']['index'] if 'index' in pdata['extra_data']['extra_pnginfo'] else 0
+        if index==0:
+            StatusInfo=''
 
-        return { "ui": { "text":"第"+str(index+1)+"次循环结果："+result} }
+        i=i+pdata['prompt'][start_id]['inputs']['stop']
+        pdata['prompt'][start_id]['inputs']['i']=i
+        pdata['extra_data']['extra_pnginfo']['index']=index+1
+        
+        result = "第"+str(index+1)+"论结束........."
+
+        if i>=total:
+            result = result+ '\n整个循环结束'
+        else:
+            r = promptFun(PromptServer.instance,pdata)
+
+        if StatusInfo.endswith('循环结束')==False and len(StatusInfo)>0:
+            result=StatusInfo+'\n'+result
+
+        pdata['extra_data']['extra_pnginfo']['StatusInfo']=result
+
+        return { "ui": { "text":result} }
 
 NODE_CLASS_MAPPINGS = {
     "ForEnd": ForEnd
