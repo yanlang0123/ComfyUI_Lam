29ea409ce68a18792257f0965f7c8cc2f490f8a2
严浪 <757078144@qq.com>
2024-08-28 22:16:42 +0800

    模型下载批处理脚本修改

--- 下载模型.bat
+++ 下载模型.bat
@@ -4,19 +4,19 @@ if exist "..\..\..\python_embeded\python.exe" (
     echo Custom Python build of ComfyUI standalone executable detected:
     echo "%RETVAL%"
     echo --------------------------------------------------
-    ..\..\..\python_embeded\python.exe  -m download.py %1
+    ..\..\..\python_embeded\python.exe download.py %1
 ) else if exist "..\..\python\python.exe" (
     echo Custom Python build of ComfyUI standalone executable detected:
     echo "%RETVAL%"
     echo --------------------------------------------------
-    ..\..\python\python.exe -m download.py %1
+    ..\..\python\python.exe download.py %1
 ) else (
     for /f "tokens=*" %%i in ('where python') do set "PYTHON_PATH=%%i" & goto :done
     :done
     echo Custom Python not found. Use system's Python executable instead:
     echo %PYTHON_PATH%
     echo --------------------------------------------------
-    python.exe  -m download.py %1
+    python.exe  download.py %1
 )
 
 :NORMALIZEPATH
