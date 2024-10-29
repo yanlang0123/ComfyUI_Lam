a7d8b8abe739eed787fca3a534d58f5b807858e5
严浪 <757078144@qq.com>
2024-10-29 18:35:16 +0800

    二维码识别升级，新增微信模型识别二维码提高识别效果

--- py/IdentifyingQR.py
+++ py/IdentifyingQR.py
@@ -1,13 +1,18 @@
-
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps, ImageDraw, ImageChops, ImageFont
 import numpy as np
+import cv2
+from cv2.wechat_qrcode import WeChatQRCode
+import os
 def tensor2pil(image):
-    return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+    return cv2.cvtColor(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8), cv2.COLOR_RGB2BGR)
 
 
 class IdentifyingQR:
     def __init__(self):
-        pass
+        modelDir = os.path.abspath(os.path.join(__file__, "../../qr_mode"))
+        self.detector = WeChatQRCode(detector_prototxt_path=modelDir+"/detect.prototxt", 
+                        detector_caffe_model_path=modelDir+"/detect.caffemodel",
+                        super_resolution_prototxt_path=modelDir+"/sr.prototxt",
+                          super_resolution_caffe_model_path=modelDir+"/sr.caffemodel")
         
     @classmethod
     def INPUT_TYPES(cls):
@@ -23,21 +28,19 @@ class IdentifyingQR:
     CATEGORY = "lam"
     
     def qr_image_2_string(self,image):
-        import cv2
-        from pyzbar.pyzbar import decode
+        scales = [0.5, 0.75, 1.0, 1.25, 1.5]
         img = tensor2pil(image)
-        strList=[]
-        msg=''
-        try:
-            # 解码二维码
-            for qrcode in decode(img):
-                print(qrcode.data.decode('utf-8'))
-                strList.append(qrcode.data.decode('utf-8'))
-            msg=','.join(strList)
-        except Exception as e:
-            print(e)
-            msg='识别异常'
-        return (msg,)
+        for scale in scales:
+            # 缩放图像
+            scaled_image = cv2.resize(img, None, fx=scale, fy=scale)
+            # 检测二维码
+            res, points = self.detector.detectAndDecode(scaled_image)
+            # 如果检测到二维码
+            if points:
+                print(f'二维码在缩放比例 {scale} 下被检测到')
+                return res
+        
+        return ('',)
     
    
     
