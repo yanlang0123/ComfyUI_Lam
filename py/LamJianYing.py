266038cc1d03697878c7941c2c0faf3815a41758
严浪 <757078144@qq.com>
2025-08-30 12:13:48 +0800

    BUG修改

--- py/LamJianYing.py
+++ py/LamJianYing.py
@@ -608,7 +608,7 @@ class JyAudio2CaptionsGroup:
             text = self.add_newlines(segments[i]["text"],row_max_size)
             start=start_at_track+segments[i]["start"]
             duration=segments[i]["end"]-segments[i]["start"]
-            captions={"subtitle": text,"font":font,"color":color,"size":size, "start_at_track": int(round(start * 100)) * 100, "duration": int(round(duration * 100)) * 100}
+            captions={"subtitle": text,"font":font,"color":color,"size":size, "start_at_track": int(round(start * 100)) * 10000, "duration": int(round(duration * 100)) * 10000}
             captions['clip_settings']=draft.Clip_settings(transform_y=transform_y,transform_x=transform_x)
             end_time=start+duration
             captions_group.append(captions)
