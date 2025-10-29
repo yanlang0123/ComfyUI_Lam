82c1af1ae81a50da27b0ce6dc41371c649ccdbe8
yanlang0123 <yanlang0123@gmail.com>
2025-10-29 11:47:46 +0800

    兼容性修改

--- py/src/LamCustomPrompt.py
+++ py/src/LamCustomPrompt.py
@@ -28,9 +28,9 @@ class CustomPromptQueue(execution.PromptQueue):
             return 
         super().put(item)
     
-    def task_done(self, item_id, history_result,status: Optional['PromptQueue.ExecutionStatus'], process_item=None):
+    def task_done(self, item_id, history_result,status: Optional['PromptQueue.ExecutionStatus'],**kwargs):
         print("结束拦截测试:",item_id)
-        return super().task_done(item_id, history_result,status,process_item)
+        return super().task_done(item_id, history_result,status,**kwargs)
     
 def get_route_keys(endKey, prompt,uniqueIds):
     keys=[]
